4625bd6445edc0df5a38bf57afa3238a0e33e777
mikehollaz <31917767+mikehollaz@users.noreply.github.com>
2017-10-03 14:52:34 +1100

    Add files via upload

---
+++ is_prime.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int is_prime(int n);
+int next_prime(int n);
+
+int main(int argc, char const *argv[]) {
+	int n, next;
+
+	printf("Enter a number: \n");
+	scanf("%d", &n);
+
+	if(is_prime(n)) {
+		printf("%d is a prime number\n",n);
+	}
+	else {
+		printf("%d is not a prime number\n",n);
+	}
+
+	next = next_prime(n);
+	printf("The next prime number is %d\n", next);
+
+	return 0;
+}
+
+int
+is_prime(int n) {
+	int divisor;
+
+	for (divisor = 2; divisor*divisor <= n; divisor++) {
+		if (n % divisor == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int
+next_prime(int n) {
+	n += 1;
+
+	if(is_prime(n)) {
+		return n;
+	}
+	return next_prime(n);
+}
+
